a33f5e9c5cfb12aecfb00bc046cd985a3b3d7603
texmynian <114612718+texmynian@users.noreply.github.com>
2022-11-20 04:30:59 +0200

    associated our db with the relevant dashboard columns and inserted some demo data

--- accounts/models.py
+++ accounts/models.py
@@ -11,6 +11,14 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     CATEGORY = (
 			('Indoor', 'Indoor'),
@@ -20,8 +28,11 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-    description = models.CharField(max_length=200, null=True)
-    date_created = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True, blank = True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
 
 class Order(models.Model):
     STATUS = (
@@ -30,7 +41,8 @@ class Order(models.Model):
             ('Delivered', 'Delivered'),
             )
     
-    # customer = 
-    # product =  
+    customer = models.ForeignKey(Customer, null=True, on_delete = models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete = models.SET_NULL) 
     date_created = models.DateTimeField(auto_now_add = True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
+    
